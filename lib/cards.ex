b7aae5ad1403fc6041a2e225a45edfff9ae47592
tony <tonyhoyinliu@gmail.com>
2020-02-14 08:05:11 -0800

    first commit

---
+++ lib/cards.ex
@@ -0,0 +1,52 @@
+defmodule Cards do
+  @moduledoc """
+  Documentation for `Cards`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Cards.hello()
+      :world
+
+  """
+  def hello do
+    # :world
+    "hi there!"
+  end
+
+  def create_deck do
+    values = ["Ace", "Two", "Three", "Four", "Five"]
+    suits = ["Spades", "Clubs", "Hearts", "Diamonds"]
+
+    # for loop in elixir
+    # for every el, do block
+    # comprehension -> mapping function
+    # gets put into brand new array
+    # cards = for value <- values do
+    #   for suit <- suits do
+    #     "#{value} of #{suit}"
+    #   end
+    # end
+    # List.flatten(cards)
+
+    for suit <- suits, value <- values do
+      # can do as many (mutiple) comprehension as you want. no need to flatten like above
+      "#{value} of #{suit}"
+    end
+  end
+
+  def shuffle(deck) do
+    Enum.shuffle(deck)
+  end
+
+  def contains?(deck, card) do
+    Enum.member?(deck, card)
+  end
+
+  def deal(deck, hand_size) do
+    Enum.split(deck, hand_size)
+  end
+end
